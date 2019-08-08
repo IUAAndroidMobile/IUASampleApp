2b2c90a870a80c5a7b2a633f280621955678be43
nicolas.fanin <nicolas.x.fanin.-nd@disney.com>
2019-08-08 10:02:51 -0300

    Dinamic Fragments

--- app/src/main/java/com/nicolasfanin/IUASampleApp/fragments/ActivityWithFragments.java
+++ app/src/main/java/com/nicolasfanin/IUASampleApp/fragments/ActivityWithFragments.java
@@ -2,6 +2,8 @@ package com.nicolasfanin.IUASampleApp.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import com.nicolasfanin.IUASampleApp.R;
 
@@ -10,6 +12,12 @@ public class ActivityWithFragments extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_mail_list);
+        setContentView(R.layout.activity_with_fragments);
+
+        //Setear un fragment en forma dinámica
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.dinamic_fragment, new MailDetailsFragment());
+        fragmentTransaction.commit();
     }
 }
