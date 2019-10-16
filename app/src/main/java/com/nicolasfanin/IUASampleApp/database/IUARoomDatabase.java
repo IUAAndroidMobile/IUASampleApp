d8692b623ef16198df2f2708641b45f6af3f4f20
nicolas.fanin <nicolas.x.fanin.-nd@disney.com>
2019-10-16 17:55:25 -0300

    Add Room Database

---
+++ app/src/main/java/com/nicolasfanin/IUASampleApp/database/IUARoomDatabase.java
@@ -0,0 +1,28 @@
+package com.nicolasfanin.IUASampleApp.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.nicolasfanin.IUASampleApp.data.Color;
+
+@Database(entities = {Color.class}, version = 1)
+public abstract class IUARoomDatabase extends RoomDatabase {
+
+    public abstract ColorDao colorDao();
+
+    private static volatile IUARoomDatabase INSTANCE;
+
+    public static IUARoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (IUARoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            IUARoomDatabase.class, "IUARoomDatabase").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
